7c3b0086912c1b7aa1f409b3137669afc9e5da1e
Anrago <141792891+Anrago@users.noreply.github.com>
2023-09-30 18:08:05 -0700

    finalized

--- Practica_5_ClasesdeAlmacenamiento/Practica_5_ClasesdeAlmacenamiento_RamosGonzalez_Antonio.cpp
+++ Practica_5_ClasesdeAlmacenamiento/Practica_5_ClasesdeAlmacenamiento_RamosGonzalez_Antonio.cpp
@@ -9,12 +9,12 @@ static int numero_ganador = 0;
 
 int msg();
 int numero_loteria();
-int iniciar_carrera();
-int tiempo(int vel);
+void iniciar_carrera();
+int velocidad();
+float tiempo(int vel);
 
 int main()
 {
-    register int coche1, coche2, coche3;
     int numero_usuario, opc;
     srand(time(NULL));
     do
@@ -50,45 +50,7 @@ int main()
             system("PAUSE");
             break;
         case 2:
-            coche1 = iniciar_carrera();
-            coche2 = iniciar_carrera();
-            coche3 = iniciar_carrera();
-            system("CLS");
-            printf("\t TIEMPOS\n");
-            printf("----------------------------\n");
-            printf("COCHE 1 velocidad: %dk/h\n", coche1);
-            printf("COCHE 1 tiempo: %ds\n", tiempo(coche1));
-            printf("----------------------------\n");
-            printf("COCHE 2 velocidad: %dk/h\n", coche2);
-            printf("COCHE 2 tiempo: %ds\n", tiempo(coche2));
-            printf("----------------------------\n");
-            printf("COCHE 3 velocidad: %dk/h\n", coche3);
-            printf("COCHE 3 tiempo: %ds\n", tiempo(coche3));
-            printf("----------------------------\n");
-            system("PAUSE");
-            system("CLS");
-            if (coche1 > coche2 && coche1 > coche3)
-            {
-                printf("***************************************\n");
-                printf("EL GANADOR ES SERGIO PEREZ(coche 1)\n");
-                printf("***************************************\n");
-            }
-            else
-            {
-                if (coche2 > coche1 && coche2 > coche3)
-                {   
-                    printf("***************************************\n");
-                    printf("EL GANADOR ES FRANCESCO VIRGOLINI(coche 2)\n");
-                    printf("***************************************\n");
-                }
-                else 
-                {
-                    printf("***************************************\n");
-                    printf("LA GANADORA ES DALIA RAMOS(coche 3)\n");
-                    printf("***************************************\n");
-                }
-            }
-            system("PAUSE");
+            iniciar_carrera();
         }
     } while (opc != 0);
     return 0;
@@ -109,21 +71,63 @@ int msg()
 
 int numero_loteria()
 {
-    static int numero_generado;
-    numero_generado = rand() % 100 + 1;
-    return numero_generado;
+    return rand() % 100 + 1;
 }
 
-int iniciar_carrera()
+void iniciar_carrera()
 {
-    int velocidad;
-    velocidad = rand() % 101 + 100;
-    return velocidad;
+    register int coche1, coche2, coche3;
+    coche1 = velocidad();
+    coche2 = velocidad();
+    coche3 = velocidad();
+    system("CLS");
+    printf("\t TIEMPOS\n");
+    printf("----------COCHE 1-----------\n");
+    printf(" velocidad: %dk/h\n", coche1);
+    printf(" tiempo: %.2fs\n", tiempo(coche1));
+    printf("----------------------------\n");
+    printf("----------COCHE 2-----------\n");
+    printf(" velocidad: %dk/h\n", coche2);
+    printf(" tiempo: %.2fs\n", tiempo(coche2));
+    printf("----------------------------\n");
+    printf("----------COCHE 3-----------\n");
+    printf(" velocidad: %dk/h\n", coche3);
+    printf(" tiempo: %.2fs\n", tiempo(coche3));
+    printf("----------------------------\n");
+    system("PAUSE");
+    system("CLS");
+    if (coche1 > coche2 && coche1 > coche3)
+    {
+        printf("***************************************\n");
+        printf("EL GANADOR ES SERGIO PEREZ(coche 1)\n");
+        printf("***************************************\n");
+    }
+    else
+    {
+        if (coche2 > coche1 && coche2 > coche3)
+        {
+            printf("***************************************\n");
+            printf("EL GANADOR ES FRANCESCO VIRGOLINI(coche 2)\n");
+            printf("***************************************\n");
+        }
+        else
+        {
+            printf("***************************************\n");
+            printf("LA GANADORA ES DALIA RAMOS(coche 3)\n");
+            printf("***************************************\n");
+        }
+    }
+    system("PAUSE");
 }
 
-int tiempo(int vel)
+int velocidad()
 {
-    int tiempo, met;
+    return rand() % 101 + 100;
+}
+
+float tiempo(int vel)
+{
+    float tiempo, met;
     met = (vel * 1000) / 3200;
 
     tiempo = 1000 / met;
